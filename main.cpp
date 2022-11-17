fc232ef12ae457ba9bae2e53afb0c9ec554cb49e
simonecampi <118023444+simonecampi@users.noreply.github.com>
2022-11-17 17:28:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@ int main() {
    cin>>a>>b>>x>>y;
    if(x<0 && y>0)
    {
-   z=a*x-b*y;
+   z=a*x-b*y
    }
    else if(x>=0 && y<=0)
    {
-   z=a*x*x-b*y;
+   z=a*x*x-b*y
    }
    else
    {
-   z=a*x+b*y*y;
+   z=a*x+b*y*y
    }
    cout<<z<<endl;
    return 0;

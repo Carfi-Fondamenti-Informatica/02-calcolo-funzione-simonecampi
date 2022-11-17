e0c3a1d9c8474cdf292b24b32594dfc751a49525
simonecampi <118023444+simonecampi@users.noreply.github.com>
2022-11-17 17:18:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,18 @@ using namespace std;
 int main() {
    float a,b,x,y,z;
    cin>>a>>b>>x>>y;
-   if(x<0 && y>0);
-   {z=a*x-b*y;}
-   else if(x>=0 && y<=0);
-   {z=a*x*x-b*y;}
+   if(x<0 && y>0)
+   {
+   z=a*x-b*y;
+   }
+   else if(x>=0 && y<=0)
+   {
+   z=a*x*x-b*y;
+   }
    else
-   {z=a*x+b*y*y;}
+   {
+   z=a*x+b*y*y;
+   }
    cout<<z<<endl;
    return 0;
 }
